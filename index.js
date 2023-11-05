533251a86b664a0576bce05c8924a8d25e84c482
RuthLopesDiniz <ruthma1964@hotmail.com>
2023-11-04 23:44:34 -0300

    Feat: validação de id via middleware adicionado

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ app.use(express.json())
 
 const orders = []
 
+const checkOrderId = (request, reponse, next)=> {
+    const { id } = request.params
+
+    const index = orders.findIndex(order =>order.id === id)//interar user /user e verificar se id existe
+
+    if(index < 0){
+        return response.status(404).json({error: "Order not found"}) //retorna resposta caso não exista
+    }
+
+    request.orderIndex = index
+    request.orderId = id
+    next()
+}
+
 //rota para listar todos os projetos incluidos
 app.get('/orders', (request, response) => {
     return response.json(orders)
@@ -20,31 +34,22 @@ const {order, clientName, price, status} = request.body
 })
 
 //rota que altera um pedido a partir da chamada do id
-app.put('/orders/:id', (request, response) => {
-    const { id } = request.params
+app.put('/orders/:id', checkOrderId, (request, response) => {
+    
     const { order, clientName, price, status} = request.body
+    const index = request.orderIndex
+    const id = request.orderId
 
     const updateOrder = {id, order, clientName, price, status}
-
-    const index = orders.findIndex(order =>order.id === id)//interar user /user
-
-    if(index < 0){
-        return response.status(404).json({mesage: "Order not found"})
-    }
 
     orders[index] = updateOrder
     return response.json(updateOrder)
 })
 
 //rota para deletar um pedido específico por id
-app.delete('/orders/:id', (request, response) => {
-    const { id } = request.params
-
-    const index = orders.findIndex(order =>order.id === id)//interar user /user
-
-    if(index < 0){
-        return response.status(404).json({mesage: "Order not found"})
-    }
+app.delete('/orders/:id', checkOrderId, (request, response) => {
+    const index = request.orderIndex
+    
     orders.splice(index, 1)
 
     return response.status(204).json()
@@ -57,7 +62,7 @@ app.get('/orders/:id', (request, response) => {
     const index = orders.find(order =>order.id === id)//interar user por user
 
     if(index < 0){
-        return response.status(404).json({mesage: "Order not found"})
+        return response.status(404).json({error: "Order not found"})
     }
 
     return response.json(index)
